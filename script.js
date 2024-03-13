7b6417faf93765facce73cc499e4c98030ab89dc
Mauricio Lugo <maurogranados31@gmail.com>
2024-03-13 14:01:06 -0500

    actualizando

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let getComputerChoice = () => {
+function getComputerChoice(){
     const randomNumber = Math.floor(Math.random() * 3);
     let computerSelection;
 
@@ -32,12 +32,18 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function playGame() {
-    console.log(playRound(prompt("Type your selection for the round 1: rocks, paper or scissors"), getComputerChoice()));
-    console.log(playRound(prompt("Type your selection for the round 2: rocks, paper or scissors"), getComputerChoice()));
-    console.log(playRound(prompt("Type your selection for the round 3: rocks, paper or scissors"), getComputerChoice()));
-    console.log(playRound(prompt("Type your selection for the round 4: rocks, paper or scissors"), getComputerChoice()));
-    console.log(playRound(prompt("Type your selection for the round 5: rocks, paper or scissors"), getComputerChoice()));
-}
+const playerSelection1 = prompt("Type your selection for round 1: rocks, paper or scissors");
+const playerSelection2 = prompt("Type your selection for round 2: rocks, paper or scissors");
+const playerSelection3 = prompt("Type your selection for round 3: rocks, paper or scissors");
+const playerSelection4 = prompt("Type your selection for round 4: rocks, paper or scissors");
+const playerSelection5 = prompt("Type your selection for round 5: rocks, paper or scissors");
 
-playGame();
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection1, computerSelection));
+console.log(playRound(playerSelection2, computerSelection));
+console.log(playRound(playerSelection3, computerSelection));
+console.log(playRound(playerSelection4, computerSelection));
+console.log(playRound(playerSelection5, computerSelection));
+
+
+
