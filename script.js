40aebce9012b435366eea308ac58fe8c10805d4a
Mauricio Lugo <maurogranados31@gmail.com>
2024-03-13 14:00:21 -0500

    Actualizando

--- script.js
+++ script.js
@@ -32,9 +32,19 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-const playerSelection = prompt("Type your selection: rocks, paper or scissors");
+const playerSelection1 = prompt("Type your selection for round 1: rocks, paper or scissors");
+const playerSelection2 = prompt("Type your selection for round 2: rocks, paper or scissors");
+const playerSelection3 = prompt("Type your selection for round 3: rocks, paper or scissors");
+const playerSelection4 = prompt("Type your selection for round 4: rocks, paper or scissors");
+const playerSelection5 = prompt("Type your selection for round 5: rocks, paper or scissors");
+
 const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+console.log(playRound(playerSelection1, computerSelection));
+console.log(playRound(playerSelection2, computerSelection));
+console.log(playRound(playerSelection3, computerSelection));
+console.log(playRound(playerSelection4, computerSelection));
+console.log(playRound(playerSelection5, computerSelection));
+
 
 
 
